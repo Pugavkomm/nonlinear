3c2fb835d0ab7c161c6895515c20702ffcbaa0dd
mechislav <slavapugavko2@gmail.com>
2017-09-23 23:28:53 +0300

    глобальный реформат проекта, добавлено: 1)удобное разделение функций 2)пространственная диаграмма Так же некоторые файлы переименованы, с целью удобства. Вызовы функций переделаны. Значения параметров по умолчанию отвечают рабочей реализации программы, но необходимо пользоваться параметрами модели

--- spatial_struct.py
+++ spatial_struct.py
@@ -4,13 +4,12 @@ import matplotlib.cm
 import numpy as np
 import function_start_ring
 
-quant_it = 400
-quant_el = 100
-elements_one = np.zeros(quant_it)
+quant_it = 400   # количество итераций
+quant_el = 100   # количество элементов
 
-matrix = function_start_ring.model_pereod(quant_el, quant_it, a = 0.05, d = .4, alpha= 1)
+matrix = function_start_ring.model_pereod(quant_el, quant_it, a = 0.05, d = .4, alpha= 1) # вызываем функцию, которая возвращает матрицу с реализацией системы
 for i in range(quant_el):
-    plt.scatter(np.arange(quant_it), i*np.ones(quant_it), 4,  c =matrix[i], alpha=0.6)
+    plt.scatter(np.arange(quant_it), i*np.ones(quant_it), 4,  c =matrix[i], alpha=0.6)  # строим график, где цветом отображаем амплитуду
 cb = plt.colorbar()
 print(min, max)
 print(np.linspace(np.min(matrix), np.max(matrix), 3))
--- system_plot.py
+++ system_plot.py
@@ -1,12 +1,13 @@
+# строим реализацию изначальной системы
 import function_start_ring
 import matplotlib.pyplot as plt
 import numpy
 
 quant_it = 500
-quant_el = 100
+quant_el = 10
 
 
-matrix = function_start_ring.model_pereod(quant_el, quant_it, alpha= 1, d = 0.4, a = .5, one_rand = 0, end_rand= 1)
+matrix = function_start_ring.model_pereod(quant_el, quant_it, alpha= 1, d = 0.4, a = .05, one_rand = 0, end_rand= 1)
 x = numpy.arange(quant_it)
 for i in range(quant_el):
     plt.plot(x, matrix[i], '.', linestyle = '-' )
